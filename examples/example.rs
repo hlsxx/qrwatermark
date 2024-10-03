7a345b307edd6c2e9507aaa5bd9a6d2d4665e753
hlsxx <holespato@gmail.com>
2024-10-03 16:07:02 +0200

    Defaults

--- examples/example.rs
+++ examples/example.rs
@@ -1,7 +1,10 @@
-use qrwatermark::QRWatermark;
+use qrwatermark::QrWatermark;
 
 fn main() {
-  let mut qrw = QRWatermark::new("Hello");
-  // qrw.print_into_console();
+  let mut qrw = QrWatermark::default();
   qrw.save_as_png("test.png").expect("Error while saving image");
+
+  // let mut qrw = QrWatermark::new("Hello");
+  // // qrw.print_into_console();
+  // qrw.save_as_png("test.png").expect("Error while saving image");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,55 @@
 use image::{ImageBuffer, Luma};
-use qrcodegen::QrCode;
+use qrcodegen::{QrCode, QrCodeEcc};
 
-pub struct QRWatermark {
-  qr_code: QrCode,
-  qr_code_image: Option<ImageBuffer<Luma<u8>, Vec<u8>>>
+struct QrCodeConfig {
+  ecc: QrCodeEcc,
 }
 
-impl QRWatermark {
+impl Default for QrCodeConfig {
+  fn default() -> Self {
+    Self {
+      ecc: QrCodeEcc::Medium
+    }
+  }
+}
 
-  pub fn new(text: &str) -> Self {
+struct QrImageConfig {
+  pixel_size: u8
+}
+
+impl Default for QrImageConfig {
+  fn default() -> Self {
+    Self {
+      pixel_size: 10
+    }
+  }
+}
+
+pub struct QrWatermarkConfig {
+  qr_code_config: QrCodeConfig,
+  qr_image_config: QrImageConfig
+}
+
+pub struct QrWatermark {
+  qr_code: QrCode,
+  qr_code_image: Option<ImageBuffer<Luma<u8>, Vec<u8>>>,
+  config: QrWatermarkConfig
+}
+
+impl QrWatermark {
+
+  pub fn new(text: &str, config: QrWatermarkConfig) -> Self {
     let qr_code = QrCode::encode_text(text, qrcodegen::QrCodeEcc::Medium).unwrap();
 
     Self {
       qr_code,
-      qr_code_image: None
+      qr_code_image: None,
+      config
     }
   }
 
   fn generate_image(&mut self) {
-    let module_size_in_pixel = 10;
+    let module_size_in_pixel = 20;
     let image_size = self.qr_code.size() * module_size_in_pixel;
 
     self.qr_code_image = Some(ImageBuffer::from_fn(image_size as u32, image_size as u32, |x, y| {
@@ -56,4 +87,20 @@ impl QRWatermark {
     Ok(())
   }
 
+}
+
+impl Default for QrWatermark {
+  fn default() -> Self {
+    let qr_code = QrCode::encode_text("Hello this is QrWatermark", qrcodegen::QrCodeEcc::Medium).unwrap();
+    let config = QrWatermarkConfig {
+      qr_code_config: QrCodeConfig::default(),
+      qr_image_config: QrImageConfig::default()
+    };
+
+    Self {
+      qr_code,
+      qr_code_image: None,
+      config
+    }
+  }
 }
