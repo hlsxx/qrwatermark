c66eccac7ac79817a70fc0ca47ac48044c4c7223
hlsxx <holespato@gmail.com>
2024-10-04 07:21:33 +0200

    Rust logo and render logo

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use image::{ImageBuffer, Luma, Rgb, Rgba};
+use image::{ImageBuffer, Luma, Pixel, Rgb, Rgba};
 use imageproc::drawing::{draw_cross_mut, Canvas};
 use qrcodegen::{QrCode, QrCodeEcc};
 
@@ -9,37 +9,45 @@ struct QrCodeConfig {
 impl Default for QrCodeConfig {
   fn default() -> Self {
     Self {
-      ecc: QrCodeEcc::Medium
+      ecc: QrCodeEcc::Quartile
     }
   }
 }
 
-struct ImageConfig {
-  pixel_size: u32
+struct ImageConfig<'a> {
+  pixel_size: u32,
+  logo_path: &'a str,
+  width: u32,
+  height: u32,
+  rgb: Rgb<u8>
 }
 
-impl Default for ImageConfig {
+impl<'a> Default for ImageConfig<'a> {
   fn default() -> Self {
     Self {
-      pixel_size: 20
+      pixel_size: 20,
+      logo_path: "imgs/rust_logo.png",
+      width: 100,
+      height: 100,
+      rgb: Rgb([0, 0, 0])
     }
   }
 }
 
-pub struct QrWatermarkConfig {
+pub struct QrWatermarkConfig<'a> {
   qr_code_config: QrCodeConfig,
-  image_config: ImageConfig
+  image_config: ImageConfig<'a>
 }
 
-pub struct QrWatermark {
+pub struct QrWatermark<'a> {
   qr_code: QrCode,
-  qr_code_image: Option<ImageBuffer<Rgba<u8>, Vec<u8>>>,
-  config: QrWatermarkConfig
+  qr_code_image: Option<ImageBuffer<Rgb<u8>, Vec<u8>>>,
+  config: QrWatermarkConfig<'a>
 }
 
-impl QrWatermark {
+impl<'a> QrWatermark<'a> {
 
-  pub fn new(text: &str, config: QrWatermarkConfig) -> Self {
+  pub fn new(text: &str, config: QrWatermarkConfig<'a>) -> Self {
     let qr_code = QrCode::encode_text(text, qrcodegen::QrCodeEcc::Medium).unwrap();
 
     Self {
@@ -50,34 +58,36 @@ impl QrWatermark {
   }
 
   fn generate_image(&mut self) {
-    let image_size = self.qr_code.size() as u32 * self.config.image_config.pixel_size;
+    let image_config = &self.config.image_config;
+    let image_size = self.qr_code.size() as u32
+      * self.config.image_config.pixel_size;
 
-    let mut image = ImageBuffer::from_fn(image_size , image_size, |x, y| {
-      let module_x = (x / self.config.image_config.pixel_size) as i32;
-      let module_y = (y / self.config.image_config.pixel_size) as i32;
-
-      let (r, g, b) = (100, 100, 100);
+    let mut image = ImageBuffer::from_fn(image_size, image_size, |x, y| {
+      let module_x = (x / image_config.pixel_size) as i32;
+      let module_y = (y / image_config.pixel_size) as i32;
 
       if self.qr_code.get_module(module_x, module_y) {
-        Rgba([r, g, b, 100])
+        image_config.rgb
+        // Rgba([1, 85, 155, 255])
         // Luma([0u8])
       } else {
-        Rgba([255, 255, 255, 100])
+        Rgb([255, 255, 255])
         // Luma([255u8])
       }
     });
 
-    let logo = image::open("logo.jpeg").unwrap();
-    let logo_thumbnail = logo.thumbnail(40, 40);
+    let logo = image::open(image_config.logo_path).unwrap();
+    let logo_thumbnail = logo.thumbnail(image_config.width, image_config.height);
 
-    // let qr_center_x = (image.size() as u32 - resized_logo.width()) / 2;
-    // let qr_center_y = (image.size() as u32 - resized_logo.height()) / 2;
-    // for x in 0..logo_thumbnail.width() {
-    //   for y in 0..logo_thumbnail.height() {
-    //     let pixel = logo_thumbnail.get_pixel(x, y);
-    //     // image.put_pixel(qr_center_x + x, qr_center_y + y, pixel.to_rgb());
-    //   }
-    // }
+    let qr_center_x = (image_size - logo_thumbnail.width()) / 2;
+    let qr_center_y = (image_size - logo_thumbnail.height()) / 2;
+
+    for x in 0..logo_thumbnail.width() {
+      for y in 0..logo_thumbnail.height() {
+        let pixel = logo_thumbnail.get_pixel(x, y).to_rgb();
+        image.put_pixel(qr_center_x + x, qr_center_y + y, pixel);
+      }
+    }
 
     // image.put_pixel(10, 10, pixel);
 
@@ -109,7 +119,7 @@ impl QrWatermark {
 
 }
 
-impl Default for QrWatermark {
+impl<'a> Default for QrWatermark<'a> {
   fn default() -> Self {
     let qr_code = QrCode::encode_text("Hello this is QrWatermark", qrcodegen::QrCodeEcc::Medium).unwrap();
     let config = QrWatermarkConfig {
