7c6894226b8e5cd944208c25b2f615eff7e8fd46
hlsxx <holespato@gmail.com>
2024-10-31 07:04:14 +0100

    Cargo

--- examples/custom.rs
+++ examples/custom.rs
@@ -7,8 +7,8 @@ fn main() {
   // Custom image config
   let image_config = ImageConfigBuilder::new()
     // .color_gradient(([206, 66, 43], [23, 23, 23])) // Custom gradient colors
-    .color([14, 99, 88])
-    .is_gradient_enabled()
+    .color([112, 81, 24])
+    // .is_gradient_enabled()
     .build();
 
   // Custom logo config
@@ -17,10 +17,10 @@ fn main() {
     .height(70)
     .build();
 
-  let mut qrw = QrWatermark::new("Custom text with a gradient")
-    .logo("imgs/rust_logo.png")
+  let mut qrw = QrWatermark::new("fcfbe5e470ec55d8d2d8dd7fc893804f")
+    .logo("imgs/tss_id_black.png")
     .logo_config(logo_config)
     .image_config(image_config);
 
-  qrw.save_as_image("./imgs/example2.png").expect("Unable to save image");
+  qrw.save_as_image("./fcfbe5e470ec55d8d2d8dd7fc893804f.png").expect("Unable to save image");
 }
